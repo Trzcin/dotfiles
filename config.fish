b4f11262957cdd9a9747331fa991d8477a01e7ec
Mikołaj Trzciński <trzccin@gmail.com>
2024-07-18 21:37:15 +0200

    new fish and neovim configs

---
+++ config.fish
@@ -0,0 +1,15 @@
+set fish_greeting
+fish_vi_key_bindings
+
+bind -M visual y fish_clipboard_copy
+bind yy fish_clipboard_copy
+bind p fish_clipboard_paste
+
+abbr -a c clear
+abbr -a l ls -g -h
+abbr -a --position command code code --ozone-platform=wayland
+
+set NVCC_PREPEND_FLAGS "-ccbin /home/linuxbrew/.linuxbrew/bin/g++-13"
+
+fish_add_path -P /home/trzcin/go/bin/
+fish_add_path -P /home/linuxbrew/.linuxbrew/bin/
