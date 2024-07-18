1ba98a2e1f2c9cb806bf4af764b95aefaacd9120
Mikołaj Trzciński <trzccin@gmail.com>
2024-07-18 21:25:52 +0200

    clean slate

--- nvim/lua/trzcin/core/theme.lua
+++
@@ -1,6 +0,0 @@
-local theme = 'catppuccin-macchiato'
-local status, _ = pcall(vim.cmd, string.format('colorscheme %s', theme))
-if not status then
-    print('Theme', theme, 'was not found')
-    return
-end
--- nvim/lua/trzcin/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-local status, comment = pcall(require, "Comment")
-if not status then
-	print("comment not found")
-	return
-end
-
-comment.setup()
