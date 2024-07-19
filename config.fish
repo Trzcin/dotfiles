a758206425e551aa70c2b23751105c6b4adbd8e2
Mikołaj Trzciński <trzccin@gmail.com>
2024-07-19 07:45:17 +0200

    add pip packages to $PATH

--- config.fish
+++ config.fish
@@ -12,4 +12,5 @@ abbr -a --position command code code --ozone-platform=wayland
 set NVCC_PREPEND_FLAGS "-ccbin /home/linuxbrew/.linuxbrew/bin/g++-13"
 
 fish_add_path -P /home/trzcin/go/bin/
+fish_add_path -P /home/trzcin/.local/bin/
 fish_add_path -P /home/linuxbrew/.linuxbrew/bin/
