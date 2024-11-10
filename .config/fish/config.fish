1be3641dbd76e53e2b3c960e6bfdb9657d68a117
Mikolaj Trzcinski <mikolaj.trzcinski0@gmail.com>
2024-11-10 15:56:20 +0100

    fish abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,10 @@ bind -M visual y fish_clipboard_copy
 bind yy fish_clipboard_copy
 bind p fish_clipboard_paste
 
+# path
 fish_add_path ~/go/bin
+
+# abbreviations
+abbr -a c clear
+abbr -a l ls -gah
+abbr -a lg lazygit
