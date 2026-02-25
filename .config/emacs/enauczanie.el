d1018bff814b197067a4e19ee4f9bbf1fe9c4e55
Mikołaj Trzciński <trzccin@gmail.com>
2026-02-25 21:02:03 +0100

    Add more courses

--- .config/emacs/enauczanie.el
+++ .config/emacs/enauczanie.el
@@ -1,6 +1,10 @@
 (defvar my/enauczanie-course-list '(
                                     ("Metody badawcze w informatyce" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=3945")
                                     ("Sieciowe systemy operacyjne" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4074")
+                                    ("Podstawy analizy rynków kapitałowych" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4686")
+                                    ("Sieci Ethernet" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4229")
+                                    ("Systemy obliczeniowe wysokiej wydajności" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4831")
+                                    ("Zarządzanie środowiskiem chmurowym" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4151")
                                     ("Zespołowy projekt badawczy" . "https://enauczanie.pg.edu.pl/2025/course/view.php?id=4323"))
     "List of enauczanie courses with the respective URLs")
 
