3d2cdb3fde79d045fc07ae4b8fc9c0b0976155d7
Mikołaj Trzciński <trzccin@gmail.com>
2026-03-14 17:13:25 +0100

    Fix welcome buffer error message

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -709,6 +709,10 @@
 (defun my/show-welcome-buffer ()
     (interactive)
     "Show *Welcome* buffer."
+    (switch-to-buffer (my/get-welcome-buffer)))
+
+(defun my/get-welcome-buffer ()
+    "Get or create *Welcome* buffer."
     (with-current-buffer (get-buffer-create "*Welcome*")
         (setq truncate-lines t)
         (setq-local evil-default-cursor '(ignore))
@@ -721,23 +725,22 @@
             (lambda (_frame)
                 (my/refresh-welcome-buffer)) nil t)
         (add-hook 'window-configuration-change-hook
-            #'my/refresh-welcome-buffer nil t)
-        (switch-to-buffer (current-buffer))
-        (message ""))
+            #'my/refresh-welcome-buffer nil t))
     (get-buffer "*Welcome*"))
 
 (defun my/refresh-welcome-buffer ()
     "Refresh welcome buffer content for WINDOW."
-    (when-let* ((inhibit-read-only t)
-                   (welcome-buffer (get-buffer "*Welcome*"))
-                   (window (get-buffer-window welcome-buffer))
-                   (image-path "~/.config/emacs/sugarcane.png")
-                   (image (create-image image-path nil nil))
-                   (image-height (cdr (image-size image)))
-                   (image-width (car (image-size image)))
-                   (top-margin (floor (/ (- (window-height window) image-height) 2)))
-                   (left-margin (floor (/ (- (window-width window) image-width) 2)))
-                   (title (format "Welcome to Emacs %s!" user-full-name)))
+    (when-let* (((display-graphic-p))
+                (inhibit-read-only t)
+                (welcome-buffer (get-buffer "*Welcome*"))
+                (window (get-buffer-window welcome-buffer))
+                (image-path "~/.config/emacs/sugarcane.png")
+                (image (create-image image-path nil nil))
+                (image-height (cdr (image-size image)))
+                (image-width (car (image-size image)))
+                (top-margin (floor (/ (- (window-height window) image-height) 2)))
+                (left-margin (floor (/ (- (window-width window) image-width) 2)))
+                (title (format "Welcome to Emacs %s!" user-full-name)))
         (with-current-buffer welcome-buffer
             (erase-buffer)
             (setq mode-line-format nil)
@@ -750,7 +753,7 @@
             (insert (propertize title 'face '(:inherit variable-pitch :height 1.2 :weight bold))))))
 
 ;; Show dashboard in new frames
-(setq initial-buffer-choice 'my/show-welcome-buffer)
+(setq initial-buffer-choice 'my/get-welcome-buffer)
 
 ;; Clear minibuffer in new frames
 (add-hook 'server-after-make-frame-hook (lambda () (message "")))
