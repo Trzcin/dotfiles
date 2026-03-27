958fec76c862b8df8455805bce6c2091474aad1a
Mikołaj Trzciński <trzccin@gmail.com>
2026-03-27 23:33:25 +0100

    Add journal.el

---
+++ .config/emacs/autoload/journal.el
@@ -0,0 +1,13 @@
+(defvar my/journal-dir
+    "~/Documents/org-notes/Journal"
+    "The directory in which journal entries will be stored.")
+
+(defun my/create-or-edit-current-journal-entry ()
+    "Switches to todays journal file if it exists or creates one if needed."
+    (interactive)
+    (let*
+        ((journal-file (f-join my/journal-dir (format-time-string "%Y-%m-%d-dziennik.org"))))
+        (find-file journal-file)
+        (when (not (file-exists-p journal-file))
+            (insert "* ")
+            (org-insert-timestamp (current-time)))))
